3f2633846e89fa6db24756695e8ac2f60422ce22
ShrenikaNataraj <shnataraj@deloitte.com>
2023-06-20 11:37:55 +0530

    order service changes

--- src/services/Order.ts
+++ src/services/Order.ts
@@ -1,5 +1,5 @@
 import { OrderDetailsInput, OrderDetailsOutput } from '../models/OrderDetails';
-import { OrderItemInput } from '../models/OrderItem';
+import { OrderItemInput, OrderItemOutput } from '../models/OrderItem';
 import db from '../models';
 import { IModalProduct, IOrderRequest } from '../types';
 
@@ -22,4 +22,20 @@ export const createOrder = async (data: IOrderRequest): Promise<any> => {
   } catch (e) {
     console.log(e);
   }
+};
+
+export const deleteOrder = async (orderID: number) => {
+  return await db.OrderDetails.destroy({ where: { o_id: orderID }, raw: true });
+};
+
+export const getOrderHistory = async (
+  email: string
+): Promise<OrderDetailsOutput[]> => {
+  return await db.OrderDetails.finAdAll({ where: { email: email }, raw: true });
+};
+
+export const getOrderDetails = async (
+  orderId: number
+): Promise<OrderItemOutput[]> => {
+  return await db.OrderItem.findAll({ where: { o_id: orderId }, raw: true });
 };
