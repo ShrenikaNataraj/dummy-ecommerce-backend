565f97481a45dbaac35b80674fe037527d0e4300
ShrenikaNataraj <shnataraj@deloitte.com>
2023-06-20 15:31:49 +0530

    removing un used imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import db from './models';
 import { routes } from './routes';
-import { getItemByKey } from './services/Product';
 
 const PORT = process.env.PORT || 3000;
 
